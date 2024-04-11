981b6d29350ce33bea277f0eb09082f6e1b7f315
slemons-sgai <slemons@smartgenix.ai>
2024-04-11 09:41:36 -0400

    Add project files.

---
+++ Maui MVVM/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Maui_MVVM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
